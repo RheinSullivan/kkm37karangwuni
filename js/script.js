fa620432c5df13071a742395e17409172f317bc2
Rhein Sullivan <126785035+RheinSullivan@users.noreply.github.com>
2024-08-02 00:04:54 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -60,3 +60,21 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+// Scroll to top button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
